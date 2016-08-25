d85fd501bdcf6799db7247a51ac8c1825f6a756a
Flame <yaolynzc@126.com>
2016-08-25 23:01:20 +0800

    add page

--- models/post.js
+++ models/post.js
@@ -58,8 +58,8 @@ Post.prototype.save = function(callback){
   });
 };
 
-//读取文章及其相关信息
-Post.getAll = function(name,callback){
+//读取文章及其相关信息 一次获取十篇文章
+Post.getTen = function(name,page,callback){
   //打开数据库
   mongodb.open(function(err,db){
     if(err){
@@ -77,20 +77,25 @@ Post.getAll = function(name,callback){
         query.name = name;
       }
 
-      //根据query对象查询文章
-      collection.find(query).sort({
-        time:-1
-      }).toArray(function(err,docs){
-        mongodb.close();
-        if(err){
-          return callback(err); //失败！返回err
-        }
-        //解析markdown为html
-        docs.forEach(function(doc){
-          doc.post = markdown.toHTML(doc.post);
+      //使用count返回特定查询的文档数 total
+      collection.count(query,function(err,total){
+        //根据query对象查询，并跳过前(page-1)*10个结果，返回之前的10个结果
+        collection.find(query,{
+          skip:(page -1 )*10,
+          limit:10
+        }).sort({
+          time:-1
+        }).toArray(function(err,docs){
+          mongodb.close();
+          if(err){
+            return callback(err);
+          }
+          //解析markdown为html
+          docs.forEach(function(doc){
+            doc.post = markdown.toHTML(doc.post);
+          });
+          callback(null,docs,total);
         });
-        
-        callback(null,docs);  //成功！以数组形式返回查询的结果
       });
     });
   });
--- routes/index.js
+++ routes/index.js
@@ -15,15 +15,21 @@ var crypto = require('crypto'),
 module.exports = function(app){
   //主页
   app.get('/',function(req,res){
-    Post.getAll(null,function(err,posts){
+    //判断是否是第一页，并把请求的页数转换成number类型
+    var page = req.query.p ? parseInt(req.query.p) : 1;
+    //查询并返回第 page 页的10篇文章
+    Post.getTen(null,page,function(err,posts,total){
       if(err){
         posts = [];
       }
 
       res.render('index',{ 
         title: '主页',
-        user:req.session.user,
         posts:posts,
+        page:page,
+        isFirstPage:(page - 1) == 0,
+        isLastPage:((page - 1)*10 + posts.length) == total,
+        user:req.session.user,
         success:req.flash('success').toString(),
         error:req.flash('error').toString()
       });
@@ -178,14 +184,16 @@ module.exports = function(app){
 
   //用户页面
   app.get('/u/:name',function(req,res){
+    var page = req.query.p ? parseInt(req.query.p) : 1;
+
     //检查用户是否存在
     User.get(req.params.name,function(err,user){
       if(!user){
         req.flash('error','用户不存在！');
-        return res.redirect('/'); //用户存在则跳转到主页
+        return res.redirect('/'); //用户不存在跳转到主页
       }
-      //查询并返回该用户的素有文章
-      Post.getAll(user.name,function(err,posts){
+      //查询并返回该用户第 page 页的10篇文章
+      Post.getTen(user.name,page,function(err,posts,total){
         if(err){
           req.flash('error',err);
           return res.redirect('/');
@@ -193,6 +201,9 @@ module.exports = function(app){
         res.render('user',{
           title:user.name,
           posts:posts,
+          page:page,
+          isFirstPage:(page - 1) == 0,
+          isLastPage:((page - 1)*10 + posts.length) == total,
           user:req.session.user,
           success:req.flash('success').toString(),
           error:req.flash('error').toString()
